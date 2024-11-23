cbeb55670fa7ea18b2267067707d420a7edb8cc0
hassanaitjabli <151209380+hassanjebli@users.noreply.github.com>
2024-11-23 01:00:51 +0100

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/shoppingCart/',
+  base: '/mini-store-learning/',
   plugins: [react()],
 })
